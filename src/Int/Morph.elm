17e49d4a2f1b0e15fc016af3198f84855273086f
lue-bird <lue.the.bird@gmail.com>
2023-07-13 22:20:30 +0200

    documentation +

--- src/Int/Morph.elm
+++ src/Int/Morph.elm
@@ -10,6 +10,15 @@ import Integer exposing (Integer)
 import Morph exposing (MorphOrError)
 
 
+{-| [`Morph.OneToOne`](Morph#OneToOne) between a [decimal representation](Integer#Integer)
+and an `Int`.
+
+Keep in mind that `Integer -> Int` can overflow
+since `Int` is fixed in bit size while [`Integer`](Integer#Integer) is not.
+
+[Inverse] of [`Integer.Morph.int`](Integer-Morph#int)
+
+-}
 integer : MorphOrError Int Integer error_
 integer =
     Morph.oneToOne Integer.toInt Integer.fromInt
