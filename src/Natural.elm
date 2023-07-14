1e736448582aa072e9e929b2e4d2db45c99941f7
lue-bird <lue.the.bird@gmail.com>
2023-07-14 16:13:04 +0200

    `Natural.fromN` correct

--- src/Natural.elm
+++ src/Natural.elm
@@ -31,7 +31,7 @@ import ArraySized
 import Bit exposing (Bit)
 import BitArray
 import BitArray.Extra
-import N exposing (Min, N, Up0, n0, n1)
+import N exposing (Min, N, Up0, n0)
 import N.Local exposing (n32)
 import RecordWithoutConstructorFunction exposing (RecordWithoutConstructorFunction)
 
@@ -40,16 +40,15 @@ import RecordWithoutConstructorFunction exposing (RecordWithoutConstructorFuncti
 Either 0 directly or a positive number represented by the bit `I` followed by at most a given count of
 [`Bit`](https://dark.elm.dmy.fr/packages/lue-bird/elm-bits/latest/Bit)s
 
-If you need a natural number representation with a specific number of bits, go
+Why does the type look like this?
+
+If you need a natural number representation with a specific number of bits, you can try
 
     ArraySized Bit (Exactly (On bitLength))
 
 For larger numbers, where you want to allow numbers of arbitrary precision,
-only `O | I ...` can enforce that `==` always gives the correct answer,
+only our version can enforce that `==` always gives the correct answer,
 since the `ArraySized` could be constructed with leading `O`s!
-
-Feel free to incorporate this into a new `type`
-with variants `NaN`, `Infinity`, ... based on your specific use-case
 
 -}
 type Natural
@@ -70,17 +69,13 @@ fromN : N range_ -> Natural
 fromN =
     \n_ ->
         case
-            n_
-                |> BitArray.fromN n32
-                |> BitArray.Extra.unpad
-                |> ArraySized.hasAtLeast n1
+            n_ |> BitArray.fromN n32 |> BitArray.Extra.unpad |> ArraySized.toList
         of
-            Err _ ->
+            [] ->
                 N0
 
-            Ok atLeast1 ->
-                AtLeast1
-                    { bitsAfterI = atLeast1 |> ArraySized.toList }
+            _ :: bitsAfterI ->
+                AtLeast1 { bitsAfterI = bitsAfterI }
 
 
 {-| Convert to a [natural number of type `N`](https://dark.elm.dmy.fr/packages/lue-bird/elm-bounded-nat/latest/)
